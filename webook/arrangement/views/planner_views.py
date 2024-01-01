2222b355f3e88b4b401981290b589732fff8f410
DESKTOP-T8VR2O2\Magnus Harlem <magnus@wemade.no>
2024-01-01 14:15:11 +0100

    fix query, remove persons

--- webook/arrangement/views/planner_views.py
+++ webook/arrangement/views/planner_views.py
@@ -19,13 +19,23 @@ from django.utils.timezone import make_aware
 from django.utils.translation import gettext_lazy as _
 from django.views import View
 from django.views.decorators.http import require_http_methods
-from django.views.generic import CreateView, DetailView, ListView, RedirectView, TemplateView, UpdateView
+from django.views.generic import (
+    CreateView,
+    DetailView,
+    ListView,
+    RedirectView,
+    TemplateView,
+    UpdateView,
+)
 from django.views.generic.edit import DeleteView, FormView
 
 from webook.arrangement.dto.event import EventDTO
 from webook.arrangement.facilities.calendar import analysis_strategies
 from webook.arrangement.forms.event_forms import CreateEventForm, UpdateEventForm
-from webook.arrangement.forms.file_forms import UploadFilesToArrangementForm, UploadFilesToEventSerieForm
+from webook.arrangement.forms.file_forms import (
+    UploadFilesToArrangementForm,
+    UploadFilesToEventSerieForm,
+)
 from webook.arrangement.forms.note_forms import CreateNoteForm, UpdateNoteForm
 from webook.arrangement.forms.ordering_forms import (
     LooselyOrderServiceForm,
@@ -36,9 +46,15 @@ from webook.arrangement.forms.ordering_forms import (
     RemovePersonFromEventForm,
     RemoveRoomFromEventForm,
 )
-from webook.arrangement.forms.planner.planner_create_arrangement_form import PlannerCreateArrangementModelForm
-from webook.arrangement.forms.planner.planner_plan_serie_form import PlannerPlanSerieForm
-from webook.arrangement.forms.planner.planner_update_arrangement_form import PlannerUpdateArrangementModelForm
+from webook.arrangement.forms.planner.planner_create_arrangement_form import (
+    PlannerCreateArrangementModelForm,
+)
+from webook.arrangement.forms.planner.planner_plan_serie_form import (
+    PlannerPlanSerieForm,
+)
+from webook.arrangement.forms.planner.planner_update_arrangement_form import (
+    PlannerUpdateArrangementModelForm,
+)
 from webook.arrangement.forms.planner_forms import AddPlannersForm, RemovePlannersForm
 from webook.arrangement.models import (
     Arrangement,
@@ -57,8 +73,14 @@ from webook.arrangement.models import (
     Room,
     RoomPreset,
 )
-from webook.arrangement.views.generic_views.archive_view import ArchiveView, JsonArchiveView
-from webook.arrangement.views.generic_views.json_form_view import JsonFormView, JsonModelFormMixin
+from webook.arrangement.views.generic_views.archive_view import (
+    ArchiveView,
+    JsonArchiveView,
+)
+from webook.arrangement.views.generic_views.json_form_view import (
+    JsonFormView,
+    JsonModelFormMixin,
+)
 from webook.authorization_mixins import PlannerAuthorizationMixin
 from webook.screenshow.models import DisplayLayout
 from webook.utils.collision_analysis import analyze_collisions
@@ -385,33 +407,40 @@ class GetArrangementsInPeriod(LoginRequiredMixin, ListView):
         with connection.cursor() as cursor:
             if db_vendor == "postgresql":
                 cursor.execute(
-                    f"""     SELECT audience.icon_class as audience_icon, arr.name as arrangement_name, audience.name as audience, audience.slug as audience_slug, (resp.first_name || ' ' || resp.last_name) as mainPlannerName,
-                                arr.id as arrangement_pk, ev.id as event_pk, arr.slug as slug, ev.title as name, ev.start as starts, arr.created as created_when, ev.association_type as association_type,
-                                ev.end as ends, loc.name as location, loc.slug as location_slug, arrtype.name as arrangement_type, arrtype.slug as arrangement_type_slug, evserie.id as evserie_id, status.name as status_name, status.color as status_color,
-                                status.slug as status_slug, ev.expected_visitors as expected_visitors,
-                                ev.buffer_after_event_id as after_buffer_ev_id, ev.buffer_before_event_id as before_buffer_ev_id,
-                                (SELECT EXISTS(SELECT id from arrangement_event WHERE buffer_before_event_id = ev.id OR buffer_after_event_id = ev.id)) AS is_rigging,
-                                array_agg( DISTINCT room.name) as room_names,
-                                array_agg( DISTINCT participants.first_name || ' ' || participants.last_name ) as people_names,
-                                (array_to_string(array_agg(DISTINCT room.slug ), ',') || ',' || array_to_string(array_agg(DISTINCT participants.slug), ',')) as slug_list
-                                from arrangement_arrangement as arr
-                                JOIN arrangement_event as ev on ev.arrangement_id = arr.id
-                                JOIN arrangement_location as loc on loc.id = arr.location_id
-                                LEFT JOIN arrangement_person as resp on resp.id = ev.responsible_id
-                                LEFT JOIN arrangement_arrangementtype as arrtype on arrtype.id = ev.arrangement_type_id
-                                LEFT JOIN arrangement_audience as audience on audience.id = ev.audience_id
-                                LEFT JOIN arrangement_statustype as status on status.id = ev.status_id
-                                LEFT JOIN arrangement_event_people as evp on evp.event_id = ev.id
-                                LEFT JOIN arrangement_person as participants on participants.id = evp.person_id
-                                LEFT JOIN arrangement_event_rooms as evr on evr.event_id = ev.id
-                                LEFT JOIN arrangement_room as room on room.id = evr.room_id
-                                LEFT JOIN arrangement_eventserie as evserie on evserie.id = ev.serie_id
-                                WHERE arr.is_archived = false AND ev.start > %s AND ev.end < %s AND ev.is_archived = false
-                                GROUP BY 
-                                    event_pk, audience.icon_class, audience.name, audience.slug,
-                                    resp.first_name, resp.last_name, arr.id, ev.id, arr.slug,
-                                    loc.name, loc.slug, arrtype.name, arrtype.slug, evserie.id,
-                                    status.name, status.color, status.slug, arr.expected_visitors
+                    f"""   SELECT audience.icon_class as audience_icon, arr.name as arrangement_name, audience.name as audience, audience.slug as audience_slug, (resp.first_name || ' ' || resp.last_name) as mainPlannerName,
+        arr.id as arrangement_pk, ev.id as event_pk, arr.slug as slug, ev.title as name, ev.start as starts, arr.created as created_when, ev.association_type as association_type,
+        ev.end as ends, loc.name as location, loc.slug as location_slug, arrtype.name as arrangement_type, arrtype.slug as arrangement_type_slug, evserie.id as evserie_id, status.name as status_name, status.color as status_color,
+        status.slug as status_slug, ev.expected_visitors as expected_visitors,
+        ev.buffer_after_event_id as after_buffer_ev_id, ev.buffer_before_event_id as before_buffer_ev_id,
+        (SELECT EXISTS(SELECT id from arrangement_event WHERE buffer_before_event_id = ev.id OR buffer_after_event_id = ev.id)) AS is_rigging,
+        array_agg( DISTINCT room.name) as  room_names,
+        array_agg( DISTINCT se.id ) as services,
+        array_agg( DISTINCT preconf.id ) as preconfigurations,
+        array_agg( DISTINCT participants.first_name || ' ' || participants.last_name ) as people_names,
+        (array_to_string(array_agg(DISTINCT room.slug ), ',') || ',' || array_to_string(array_agg(participants.slug), ',')) as slug_list
+        from arrangement_arrangement as arr
+        JOIN arrangement_event as ev on ev.arrangement_id = arr.id
+        JOIN arrangement_location as loc on loc.id = arr.location_id
+        LEFT JOIN arrangement_person as resp on resp.id = ev.responsible_id
+        LEFT JOIN arrangement_arrangementtype as arrtype on arrtype.id = ev.arrangement_type_id
+        LEFT JOIN arrangement_audience as audience on audience.id = ev.audience_id
+        LEFT JOIN arrangement_statustype as status on status.id = ev.status_id
+        LEFT JOIN arrangement_event_rooms as evr on evr.event_id = ev.id
+        LEFT JOIN arrangement_room as room on room.id = evr.room_id
+        LEFT JOIN arrangement_serviceorder_events as so_e_link on so_e_link.event_id = ev.id
+        LEFT JOIN arrangement_serviceorder as so on so.id = so_e_link.serviceorder_id
+        LEFT JOIN arrangement_service as se on se.id = so.service_id
+        LEFT JOIN arrangement_serviceorderpreconfiguration as preconf on preconf.id = so.applied_preconfiguration_id
+        LEFT JOIN arrangement_serviceorderprovision as sopr on sopr.for_event_id = ev.id
+        LEFT JOIN arrangement_serviceorderprovision_selected_personell as s_pers on s_pers.serviceorderprovision_id = sopr.id
+        LEFT JOIN arrangement_person as participants on s_pers.person_id = participants.id
+        LEFT JOIN arrangement_eventserie as evserie on evserie.id = ev.serie_id
+        WHERE arr.is_archived = false AND ev.start > %s AND ev.end < %s AND ev.is_archived = false
+        GROUP BY 
+            event_pk, audience.icon_class, audience.name, audience.slug,
+            resp.first_name, resp.last_name, arr.id, ev.id, arr.slug,
+            loc.name, loc.slug, arrtype.name, arrtype.slug, evserie.id,
+            status.name, status.color, status.slug, arr.expected_visitors
                             """,
                     [start, end],
                 )

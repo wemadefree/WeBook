20adff4d274a723f0476f36db5e3ddcc230fc7ba
DESKTOP-T8VR2O2\Magnus Harlem <magnus@wemade.no>
2024-01-01 16:22:52 +0100

    add entrypoint.sh to Dockerfile.cloudrun

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 sleep 5  # This is just to ensure the postgres is fully up and running.
-python manage.py makemigrations
-python manage.py migrate
 python manage.py collectstatic --noinput
 exec "$@"

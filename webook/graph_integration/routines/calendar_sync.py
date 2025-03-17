76f3f550bc4f797aaad945a5fb0a018c81e2fcb6
Magnus <magnus@wemade.no>
2025-03-17 21:33:03 +0100

    escape hatch for calendar create retry

--- webook/graph_integration/routines/calendar_sync.py
+++ webook/graph_integration/routines/calendar_sync.py
@@ -702,7 +702,7 @@ async def subscribe_person_to_webook_calendar(person: Person) -> GraphCalendar:
                 await create_graph_service_client()
                 .users.by_user_id(person.social_provider_email)
                 .calendars.post(
-                    Calendar(name=settings.APP_TITLE + " --- " + person.full_name)
+                    Calendar(name=settings.APP_TITLE + " - " + person.full_name)
                 )
             )
         except ODataError as err:
@@ -744,6 +744,12 @@ async def subscribe_person_to_webook_calendar(person: Person) -> GraphCalendar:
                             if err.error.code != "ErrorFolderExists":
                                 raise err
                             c += 1
+
+                            if c > 100:
+                                raise ValueError(
+                                    "Could not create calendar for person"
+                                )
+                            
 
             raise err
 

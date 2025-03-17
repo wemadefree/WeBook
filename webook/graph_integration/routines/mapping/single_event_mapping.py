3173c266171d60dc7fb0f7af0411ed55e875d5b3
Magnus <magnus@wemade.no>
2025-03-17 21:24:26 +0100

    adjustments to single_event_mapping, async stuff handling

--- webook/graph_integration/routines/mapping/single_event_mapping.py
+++ webook/graph_integration/routines/mapping/single_event_mapping.py
@@ -1,6 +1,7 @@
 from typing import Coroutine, List
 from msgraph.generated.models.event import Event as GraphEvent
 from webook.arrangement.models import (
+    Arrangement as WebookArrangement,
     Event as WebookEvent,
     PlanManifest as WebookSerieManifest,
     Location as WebookLocation,
@@ -32,25 +33,26 @@ async def map_event_to_graph_event(event: WebookEvent) -> GraphEvent:
     room_names = ", ".join(
         [room.name for room in await sync_to_async(list)(event.rooms.all())]
     )
+    arrangement = await WebookArrangement.objects.aget(pk=event.arrangement_id)
+    location = await WebookLocation.objects.aget(pk=arrangement.location_id)
+
     return GraphEvent(
         subject=event.title,
         body=ItemBody(content=event.title, content_type=BodyType.Html),
         start=DateTimeTimeZone(date_time=event.start.isoformat(), time_zone="UTC"),
         end=DateTimeTimeZone(date_time=event.end.isoformat(), time_zone="UTC"),
-        location=GraphLocation(
-            display_name=f"{event.arrangement.location.name} ({room_names})"
-        ),
+        location=GraphLocation(display_name=f"{location.name} ({room_names})"),
         # TODO: If we create one event per person, will the people get the event duplicated?
         # This needs to be tested.
         attendees=[
             Attendee(
                 email_address=EmailAddress(
-                    address=person.social_provider_email,
+                    address="magnus@wemade.no",
                     name=person.full_name,
                 ),
                 type="required",
             )
-            for person in event.people.all()
+            async for person in event.people.all()
         ],
         allow_new_time_proposals=False,
     )

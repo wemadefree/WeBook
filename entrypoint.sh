6c6cc2890621b7dfae302ecc9fd7b4b184aca642
Magnus <magnus@wemade.no>
2025-06-24 17:09:19 +0200

    without collectstatic

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 sleep 5  # This is just to ensure the postgres is fully up and running.
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic --noinput
+# python manage.py collectstatic --noinput
 exec "$@"

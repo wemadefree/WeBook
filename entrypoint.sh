f68ff8141dcbc4d1f5407d6a310667d3733a1b04
Magnus <magnus@wemade.no>
2025-06-24 14:49:59 +0200

    test with just entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 \#!/bin/sh
 sleep 5  # This is just to ensure the postgres is fully up and running.
-# python manage.py makemigrations
-# python manage.py migrate
+python manage.py makemigrations
+python manage.py migrate
 python manage.py collectstatic --noinput
-exec "$@"
+# exec "$@"
+gunicorn --preload -b 0.0.0.0:8080 config.wsgi:application --threads 6 -w 6

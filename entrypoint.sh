ba7d53ed9a99dc417a4536365414e2f7ef64168b
Magnus <magnus@wemade.no>
2025-06-24 16:58:40 +0200

    revert back to old configuration for dockerfile.cloudrun and entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,4 @@ sleep 5  # This is just to ensure the postgres is fully up and running.
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
-# exec "$@"
-gunicorn --preload -b 0.0.0.0:8080 config.wsgi:application --threads 8 -w 1
+exec "$@"

dc0e548fa54944e3a1a02872fd8cfa39cdb9d9e0
Magnus <magnus@wemade.no>
2025-06-25 09:38:16 +0200

    update all persons where possible in management commnad

--- webook/users/management/commands/update_emails_to_match_social_accounts.py
+++ webook/users/management/commands/update_emails_to_match_social_accounts.py
@@ -19,6 +19,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         dry_run = options["dry_run"]
         users: List[User] = User.objects.all()
+        people: List[Person] = Person.objects.all()
 
         for user in users:
             social_accounts: List[SocialAccount] = list(user.socialaccount_set.all())
@@ -49,11 +50,33 @@ class Command(BaseCommand):
                     f"Updating email for user {user.id} from {user.email} to {social_account_email}"
                 )
                 if not dry_run:
-                    user.email = social_account_email
-                    user.save()
+                    try:
+                        user.email = social_account_email
+                        user.save()
 
-                    person = Person.objects.filter(user=user).first()
-                    if person:
-                        person.personal_email = social_account_email
-                        person.social_provider_email = social_account_email
+                        person = Person.objects.filter(user=user).first()
+                        if person:
+                            person.personal_email = social_account_email
+                            person.social_provider_email = social_account_email
+                            person.save()
+                    except Exception as e:
+                        print(f"Failed to update email for user {user.id}: {str(e)}")
+
+        for person in people:
+            user = person.user_set.first()
+            if not user:
+                print(f"Person {person.id} has no associated user")
+                continue
+
+            if person.social_provider_id and person.social_provider_email != user.email:
+                person.social_provider_email = user.email
+                print(
+                    f"Updating social_provider_email for person {person.id} to {user.email}"
+                )
+                if not dry_run:
+                    try:
                         person.save()
+                    except Exception as e:
+                        print(
+                            f"Failed to update social_provider_email for person {person.id}: {str(e)}"
+                        )
